0a2ea0ee4fa3f980f3522fe3c2927d7baf55d691
Sajal Ghosh <sajalghosh.itrd@gmail.com>
2023-03-16 23:51:36 +0530

    5th commit

--- src/Component/CoinInfo.js
+++ src/Component/CoinInfo.js
@@ -1,11 +1,96 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { CryptoState } from "../CryptoContext";
+import { Line } from "react-chartjs-2";
+import { HistoricalChart } from "../Config/Config";
 
-const CoinInfo = () => {
+import Chart from 'chart.js/auto';
+import { CategoryScale } from 'chart.js';
+import '../CssFile/CoinInfo.css';
+import { chartDays } from "../ButtonData";
+
+
+
+
+
+const CoinInfo = ({ coin }) => {
+  Chart.register(CategoryScale);
+  const [historicData, setHistoricData] = useState();
+  const [days, setDays] = useState(1);
+  const { currency } = CryptoState();
+  // const [flag,setflag] = useState(false);
+
+  
+
+
+  const fetchHistoricData = async () => {
+    try {
+      if (!coin.id) {
+        throw new Error('Coin ID is not defined');
+      }
+      const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+      setHistoricData(data.prices);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(historicData);
+
+  useEffect(() => {
+    fetchHistoricData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[coin.id]);
+
+  console.log(historicData);
+
+  
+
   return (
-    <div>
-      coin
-    </div>
-  )
-}
+    
+          <div>
+          <div className='chart'>
+         <Line
+              data={{
+                labels: historicData?.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                      : `${date.getHours()}:${date.getMinutes()} AM`;
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
 
-export default CoinInfo
+                datasets: [
+                  {
+                    data: historicData?.map((coin) => coin[1]),
+                    label: `Price ( Past ${days} Days ) in ${currency}`,
+                    borderColor: "#EEBC1D",
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  point: {
+                    radius: 1,
+                  },
+                },
+              }}
+            /> 
+            </div>
+
+            <div className='coininfo__buttons'>
+            {chartDays.map((day) =>(
+              <button>{day.label}</button>
+            ))}
+
+
+            </div>
+        
+            
+          </div>
+        
+  );
+};
+
+export default CoinInfo;
--- src/Component/CoinPage.jsx
+++ src/Component/CoinPage.jsx
@@ -3,13 +3,17 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { SingleCoin } from '../Config/Config';
 import { CryptoState } from '../CryptoContext';
-import CoinInfo from '../Component/CoinInfo';
+import CoinInfo from '../Component/CoinInfo'
 import '../CssFile/CoinPage.css'
+
+
 
 const CoinPage = () => {
 
   const { id } = useParams();
   const [coin, setCoin] = useState([])
+
+ 
 
   const { currency, symbol } = CryptoState();
 
@@ -18,7 +22,10 @@ const CoinPage = () => {
 
     setCoin(data)
   }
-  console.log(coin);
+
+ 
+  
+  
   function numberWithCommas(x) {
     return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
@@ -29,27 +36,38 @@ const CoinPage = () => {
     fetchCoin();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+  // console.log(coin);
+  
+
+ 
 
   return (
     <div className="coinpage__container">
-      <div className='coinpage__sidebar'>
-        <img className='sidebar__image' src={coin?.image?.large} alt={coin?.id} />
-        <h3 className='sidebar__heading' >{coin?.name}</h3>
-        <p className='sidebar__description'>{coin?.description?.en.split(". ")[0]}</p>
-        <div className='market__data'>
-         <div className='rank'> Rank : {coin?.coingecko_rank}</div>
-         <div className='current__price'> Current Price : {numberWithCommas(coin?.market_data?.current_price[currency.toLowerCase()])}</div>
-         <div className='matket__cap'> Market Cap : &nbsp;  
-         {symbol} {" "} {numberWithCommas(coin?.market_data?.market_cap[currency.toLowerCase()])}</div>
+      
+
+        <div className='coinpage__leftsidebar'>
+          <img className='sidebar__image' src={coin?.image?.large} alt={coin?.id} />
+          <h3 className='sidebar__heading' >{coin?.name}</h3>
+          <p className='sidebar__description'>{coin?.description?.en.split(". ")[0]}</p>
+          <div className='market__data'>
+            <div className='rank'> Rank : {coin?.coingecko_rank}</div>
+            <div className='current__price'> Current Price : {numberWithCommas(coin?.market_data?.current_price[currency.toLowerCase()])}</div>
+            <div className='matket__cap'> Market Cap : &nbsp;
+              {symbol} {" "} {numberWithCommas(coin?.market_data?.market_cap[currency.toLowerCase()])}</div>
+          </div>
+
+        </div>
+        <div className='coinpage__rightsidebar'>
+        <CoinInfo  coin={coin}/>
+            
+
         </div>
 
+
+
       </div>
-      <CoinInfo/>
 
+      )
+      }
 
-
-    </div>
-  )
-}
-
-export default CoinPage
+      export default CoinPage
